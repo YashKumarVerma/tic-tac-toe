ef1516e50aec5b7652f6b67ce5a4edf8b7383f5a
eeshashetty <ekshetty2410@gmail.com>
2020-07-31 20:24:00 +0530

    shifted multiplayer to testing

--- src/App.js
+++ src/App.js
@@ -1,50 +1,26 @@
 import React, { Component } from 'react';
 import TicTacToe from './tictactoe';
-import Multiplayer from './multiplayer'
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 
 class App extends Component {
   render() {
   return (
-    <Router>
     <div className="App">
-      <Switch>
-        <Route path = "/multiplayer">
-          <Multiplayer />
-        </Route>
-      </Switch>
-      <Route path = "/bot">
-          <TicTacToe/>
-      </Route>
+      <TicTacToe/>
       <Display />
-    </div>
-
-    </Router>
+      </div>
   );
 }
 }
 function Display () {
     return (
-    <Router>
-      <div className="App">
       <div className="display">
       <div className="header">
         Tic-Tac-Toe
       </div>  
       <div className="links">by <a href="https://eeshashetty.github.io" target="_blank" rel="noopener noreferrer">@eeshashetty</a></div>
-      <div className="buttons">
-      <Link to = "/multiplayer"><button className="btn left">Multiplayer</button></Link>
-      <Link to = "/bot"><button className="btn right">Bot Mode 🤖</button></Link>
+      <div className="msg">~ the first move takes a while to load, pls bear with it ~</div>
       </div>
-      </div>
-            </div>
-    </Router>
     );
   }
 
